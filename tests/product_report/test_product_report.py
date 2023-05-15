536ab3058c93f3574a6639d88bfce444e196cebc
Beterraba <ls6182315@gmail.com>
2023-05-15 00:44:31 -0300

    test product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+                    1,
+                    "Farinha",
+                    "Dona Benta",
+                    "01-05-2021",
+                    "02-06-2023",
+                    "X567H2",
+                    "ao abrigo de luz")
+
+    assert (
+        produto.__repr__() ==
+        "O produto Farinha fabricado em 01-05-2021 por Dona Benta com" +
+        " validade até 02-06-2023 precisa ser armazenado ao abrigo de luz.")
+
+
