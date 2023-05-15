fe3faa6314734b4cced32430819d8e5242dfaed1
Beterraba <ls6182315@gmail.com>
2023-05-15 00:27:57 -0300

    fix

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -16,25 +16,19 @@ class CompleteReport(SimpleReport):
     @staticmethod
     def get_stock_by_company(data):
         """Método que retorna os produtos estocados por empresa"""
-        companies = []
-        tupled_companies = []
-        for item in data:
-            companies.append(item["nome_da_empresa"])
-        companies = set(companies)
-        stock_by_company = ""
-        for company in companies:
-            stock = CompleteReport.get_stock(data, company)
-            tupled_companies.append((company, stock))
-        tupled_companies.sort(key=lambda x: x[0])
-        for company in tupled_companies:
-            stock_by_company += f"- {company[0]}: {company[1]}\n"
-        return stock_by_company
+        lista = CompleteReport.get_stock(data)
+        stock = ""
+        for company, counter in lista.items():
+            stock += f"- {company}: {counter}\n"
+        return stock
 
     @staticmethod
-    def get_stock(data, company):
+    def get_stock(data):
         """Método que retorna a quantidade de produtos estocados por empresa"""
-        stock = 0
+        stock = {}
         for item in data:
-            if item["nome_da_empresa"] == company:
-                stock += 1
+            if item["nome_da_empresa"] in stock:
+                stock[item["nome_da_empresa"]] += 1
+            else:
+                stock[item["nome_da_empresa"]] = 1
         return stock
